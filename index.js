a77bf23e520ebb3b9de2f2cad5251f569b8e384e
Ryan Haack <ryan@haackr.com>
2019-09-17 11:59:49 -0600

    Move flight data to json file and verify check in is in the future before sceduling task.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const schedule = require('node-schedule');
+const fs = require('fs');
 
 const CHECKIN_URL = 'https://www.southwest.com/air/check-in/index.html';
 const CONFIRMATION_FIELD_SELECTOR = '#confirmationNumber';
@@ -9,48 +10,68 @@ const CONFIRM_BUTTON_SELECTOR = '#form-mixin--submit-button';
 
 let jobs = [];
 
-const flights = [
-  // {
-  //   firstName: 'Test',
-  //   lastName: 'Testerson',
-  //   confirmationNumber: 'A1BCDE',
-  //   flightTime: new Date(Date.UTC(2019, 7, 18, 17, 23)),
-  //   returnFlightTime: new Date(Date.UTC(2019, 7, 18, 17, 25)),
-  // },
-  {
-  firstName: 'Ryan',
-  lastName: 'Haack',
-  confirmationNumber: 'R5DNAZ',
-  flightTime: new Date(Date.UTC(2019, 8, 22, 16, 15)),
-  returnFlightTime: new Date(Date.UTC(2019, 8, 27, 17, 05)),
-  },
-];
+const flights = JSON.parse(fs.readFileSync('flights.json'));
 
-const scheduleFlights = (flights) => {
-  flights.forEach(flight => {scheduleConfirmation(flight)});
-}
+const scheduleFlights = flights => {
+  flights.forEach(flight => {
+    scheduleConfirmation(flight);
+  });
+};
 
-const scheduleConfirmation = (flight) => {
+const scheduleConfirmation = flight => {
   // subtract 24 hours from the flight time
-  departCheckInTime = new Date(flight.flightTime - 1000 * 60 * 60 * 24);
-  console.log('Check in departing flight at: ', departCheckInTime.toDateString(), departCheckInTime.toTimeString());
-  const departFl = schedule.scheduleJob(departCheckInTime, () => {confirmFlight(flight.firstName, flight.lastName, flight.confirmationNumber)});
-  // console.log(departFl);
-  jobs.push(departFl);
-  if(flight.returnFlightTime) {
-    returnCheckInTime = new Date(flight.returnFlightTime - 1000 * 60 * 60 * 24);
-    console.log('Check in returning flight at: ', returnCheckInTime.toDateString(), returnCheckInTime.toTimeString());
-    const returnFl = schedule.scheduleJob(returnCheckInTime, () => {confirmFlight(flight.firstName, flight.lastName, flight.confirmationNumber)});
-    // console.log(returnFl);
-    jobs.push(returnFl);
+  departCheckInTime = new Date(
+    new Date(flight.flightTime) - 1000 * 60 * 60 * 24
+  );
+  if (departCheckInTime > Date.now()) {
+    console.log(
+      'Check in departing flight at: ',
+      departCheckInTime.toDateString(),
+      departCheckInTime.toTimeString()
+    );
+    const departFl = schedule.scheduleJob(departCheckInTime, () => {
+      confirmFlight(
+        flight.firstName,
+        flight.lastName,
+        flight.confirmationNumber
+      );
+    });
+    // console.log(departFl);
+    jobs.push(departFl);
   }
-}
+  if (flight.returnFlightTime) {
+    returnCheckInTime = new Date(
+      new Date(flight.returnFlightTime) - 1000 * 60 * 60 * 24
+    );
+    if (returnCheckInTime > Date.now()) {
+      console.log(
+        'Check in returning flight at: ',
+        returnCheckInTime.toDateString(),
+        returnCheckInTime.toTimeString()
+      );
+      const returnFl = schedule.scheduleJob(returnCheckInTime, () => {
+        confirmFlight(
+          flight.firstName,
+          flight.lastName,
+          flight.confirmationNumber
+        );
+      });
+      // console.log(returnFl);
+      jobs.push(returnFl);
+    }
+  }
+};
 
 const confirmFlight = async (firstName, lastName, confirmationNumber) => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(CHECKIN_URL);
-  const [confirmationNumberInput, firstNameInput, lastNameInput, button] = await Promise.all([
+  const [
+    confirmationNumberInput,
+    firstNameInput,
+    lastNameInput,
+    button,
+  ] = await Promise.all([
     page.$(CONFIRMATION_FIELD_SELECTOR),
     page.$(FIRST_NAME_SELECTOR),
     page.$(LAST_NAME_SELECTOR),
@@ -61,7 +82,7 @@ const confirmFlight = async (firstName, lastName, confirmationNumber) => {
   await lastNameInput.type(lastName);
   await button.click();
   await page.waitFor(5000);
-  // await page.screenshot({ path: 'example.png' });
+  await page.screenshot({ path: 'example.png' });
   await browser.close();
 };
 
